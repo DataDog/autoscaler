f755fcd61fa5fc1c6fb17dfa04f66720bf627c56
Marc Joseph Szalkiewicz <marc.szalkiewicz@datadoghq.com>
2019-05-24 16:24:48 -0400

    [cluster-autoscaler-1.14.2-opentracing] push images in build script

--- apm/build.sh
+++ apm/build.sh
@@ -7,3 +7,14 @@ IMAGE="autoscaler-dependency-builder:latest"
 docker build --compress -t "${IMAGE}" -f apm/Dockerfile .
 docker run --rm -v "${REPO}/cluster-autoscaler:/tmp/out" "${IMAGE}" sh -c "cp cluster-autoscaler /tmp/out/"
 ( cd "${REPO}/cluster-autoscaler" && make make-image )
+
+VERSION="apm2"
+
+for CLOUD_IMAGE in \
+    727006795293.dkr.ecr.us-east-1.amazonaws.com/cluster-autoscaler:v1.14.2-"${VERSION}" \
+    gcr.io/datadog-staging/cluster-autoscaler:v1.14.2-"${VERSION}" \
+    gcr.io/datadog-prod/cluster-autoscaler:v1.14.2-"${VERSION}"
+do
+    docker tag staging-k8s.gcr.io/cluster-autoscaler:dev "${CLOUD_IMAGE}"
+    docker push "${CLOUD_IMAGE}"
+done
