isabella232/autoscaler-1

cluster-autoscaler/balance: balance across zones
Signed-off-by: Julien Balestra <julien.balestra@datadoghq.com>

cluster-autoscaler/cloudprovider/aws/aws_cloud_provider.go
@@ -325,8 +325,8 @@ func (ng *AwsNodeGroup) TemplateNodeInfo() (*schedulernodeinfo.NodeInfo, error) 
 	if err != nil {
 		return nil, err
 	}
-
-	nodeInfo := schedulernodeinfo.NewNodeInfo(cloudprovider.BuildKubeProxy(ng.asg.Name))
+	var pods []*apiv1.Pod
+	nodeInfo := schedulernodeinfo.NewNodeInfo(pods...)
 	nodeInfo.SetNode(node)
 	return nodeInfo, nil
 }

DataDog/autoscaler

cluster-autoscaler/cloudprovider/aws/aws_cloud_provider.go
@@ -326,7 +326,8 @@ func (ng *AwsNodeGroup) TemplateNodeInfo() (*schedulerframework.NodeInfo, error)
 		return nil, err
 	}
 
-	nodeInfo := schedulerframework.NewNodeInfo(cloudprovider.BuildKubeProxy(ng.asg.Name))
+	var pods []*apiv1.Pod
+	nodeInfo := schedulerframework.NewNodeInfo(pods...)
 	nodeInfo.SetNode(node)
 	return nodeInfo, nil
 }