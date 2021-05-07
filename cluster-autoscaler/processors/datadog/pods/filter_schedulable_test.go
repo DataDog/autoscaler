730c81ec545c769eddf6fe5e686c4bb04d489113
Benjamin Pineau <benjamin.pineau@datadoghq.com>
2021-05-07 16:55:22 +0200

    [local] Introduce a PodListProcessor frontend
    This is only moving existing code around (not functional changes)
    in order to introduce a processor frontend (processor.go) that allows
    chaining filters and transformations.
    
    While at it, bring unit tests for our local changes.
    
    New functionalities in follow-up PRs.

--- cluster-autoscaler/processors/datadog/pods/filter_schedulable_test.go
+++ cluster-autoscaler/processors/datadog/pods/filter_schedulable_test.go
@@ -113,7 +113,7 @@ func TestFilterOutSchedulableByPacking(t *testing.T) {
 				assert.NoError(t, err)
 			}
 
-			filterOutSchedulablePodListProcessor := NewFilterOutSchedulablePodListProcessor()
+			filterOutSchedulablePodListProcessor := NewFilterOutSchedulable()
 
 			err = clusterSnapshot.Fork()
 			assert.NoError(t, err)
@@ -258,7 +258,7 @@ func BenchmarkFilterOutSchedulableByPacking(b *testing.B) {
 				b.ResetTimer()
 
 				for i := 0; i < b.N; i++ {
-					filterOutSchedulablePodListProcessor := NewFilterOutSchedulablePodListProcessor()
+					filterOutSchedulablePodListProcessor := NewFilterOutSchedulable()
 					if stillPending, err := filterOutSchedulablePodListProcessor.filterOutSchedulableByPacking(pendingPods, clusterSnapshot, predicateChecker); err != nil {
 						assert.NoError(b, err)
 					} else if len(stillPending) < tc.pendingPods {
