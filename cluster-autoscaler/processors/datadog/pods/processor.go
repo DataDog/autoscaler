60785f74242e1946b1a6ee4752ede846e6f6c457
Benjamin Pineau <benjamin.pineau@datadoghq.com>
2021-05-07 16:55:22 +0200

    [local only] Introduce a PodListProcessor frontend
    This is only moving existing code around (not functional changes)
    in order to introduce a processor frontend (processor.go) that allows
    chaining filters and transformations.
    
    While at it, bring unit tests for our local changes.
    
    New functionalities in follow-up PRs.

---
+++ cluster-autoscaler/processors/datadog/pods/processor.go
@@ -0,0 +1,56 @@
+package pods
+
+import (
+	"time"
+
+	"k8s.io/autoscaler/cluster-autoscaler/context"
+	"k8s.io/autoscaler/cluster-autoscaler/metrics"
+	proc "k8s.io/autoscaler/cluster-autoscaler/processors/pods"
+
+	apiv1 "k8s.io/api/core/v1"
+	klog "k8s.io/klog/v2"
+)
+
+type filteringPodListProcessor struct {
+	transforms []proc.PodListProcessor
+	filters    []proc.PodListProcessor
+}
+
+func NewFilteringPodListProcessor() *filteringPodListProcessor {
+	return &filteringPodListProcessor{
+		transforms: []proc.PodListProcessor{
+			NewTransformLocalData(),
+		},
+		filters: []proc.PodListProcessor{
+			NewFilterOutSchedulable(),
+		},
+	}
+}
+
+func (p *filteringPodListProcessor) CleanUp() {}
+
+func (p *filteringPodListProcessor) Process(ctx *context.AutoscalingContext, pending []*apiv1.Pod) ([]*apiv1.Pod, error) {
+	klog.V(4).Infof("Filtering pending pods")
+	start := time.Now()
+
+	var err error
+
+	for _, transform := range p.transforms {
+		pending, err = transform.Process(ctx, pending)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	unschedulablePodsToHelp := make([]*apiv1.Pod, len(pending))
+	copy(unschedulablePodsToHelp, pending)
+	for _, filter := range p.filters {
+		unschedulablePodsToHelp, err = filter.Process(ctx, unschedulablePodsToHelp)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	metrics.UpdateDurationFromStart(metrics.FilterOutSchedulable, start)
+	return unschedulablePodsToHelp, nil
+}
