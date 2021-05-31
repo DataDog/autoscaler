cfde08798566f233c361c0db27ef3f6157060321
Benjamin Pineau <benjamin.pineau@datadoghq.com>
2021-05-31 23:04:46 +0200

    [local] Define Datadog custom variables and accessors
    This is meant to isolate, contain and wrap access to our custom
    resource (storageclass/local-data) and label (/local-storage)
    in a single place, separated in our processors/datadog/ namespace.
    
    Avoid spreading local names and constraints over all the code base.

---
+++ cluster-autoscaler/processors/datadog/common/common.go
@@ -0,0 +1,84 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package common
+
+import (
+	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+)
+
+const (
+	// DatadogLocalStorageLabel is "true" on nodes offering local storage
+	DatadogLocalStorageLabel = "nodegroups.datadoghq.com/local-storage"
+
+	// DatadogLocalDataResource is a virtual resource placed on new or future
+	// nodes offering local storage, and currently injected as requests on
+	// Pending pods having a PVC for local-data volumes.
+	DatadogLocalDataResource apiv1.ResourceName = "storageclass/local-data"
+)
+
+var (
+	// DatadogLocalDataQuantity is the default amount of DatadogLocalDataResource
+	DatadogLocalDataQuantity = resource.NewQuantity(1, resource.DecimalSI)
+)
+
+// NodeHasLocalData returns true if the node holds a local-storage:true label
+func NodeHasLocalData(node *apiv1.Node) bool {
+	if node == nil {
+		return false
+	}
+	value, ok := node.GetLabels()[DatadogLocalStorageLabel]
+	return ok && value == "true"
+}
+
+// ReducedNodeInfo is a reduced NodeInfo interface mean to requires just what's
+// needed by SetNodeLocalDataResource(), and remain compatible with both
+// k8s.io/kubernetes/pkg/scheduler/framework NodeInfo object and with
+// k8s.io/kube-scheduler/framework NodeInfo interface.
+type ReducedNodeInfo interface {
+	Node() *apiv1.Node
+	SetNode(node *apiv1.Node)
+}
+
+// SetNodeLocalDataResource updates a NodeInfo with the DatadogLocalDataResource resource
+func SetNodeLocalDataResource(nodeInfo ReducedNodeInfo) {
+	node := nodeInfo.Node()
+	if node == nil {
+		return
+	}
+
+	if node.Status.Allocatable == nil {
+		node.Status.Allocatable = apiv1.ResourceList{}
+	}
+	if node.Status.Capacity == nil {
+		node.Status.Capacity = apiv1.ResourceList{}
+	}
+
+	node.Status.Capacity[DatadogLocalDataResource] = DatadogLocalDataQuantity.DeepCopy()
+	node.Status.Allocatable[DatadogLocalDataResource] = DatadogLocalDataQuantity.DeepCopy()
+
+	// Even though we get a pointer to the original node (and the update above
+	// changes it in place), we still need to call SetNode() in order to trigger
+	// an update of the NodeInfo concrete implementation: the implem provided by
+	// k8s.io/kubernetes/pkg/scheduler/framework maintains an Allocatable field
+	// at SetNode() time, but we don't have acess to that field as we only have
+	// access to a reduced NodeInfo interface returned by ClusterSnapshot.List().
+	// We can't RemoveNode() out of extra caution as we did previously (with older
+	// Autoscaler codebases when ClusterSnapshot.List() returned concrete nodeInfo
+	// implementations, because the NodeInfo _interface_ has no RemoveNode() method.
+	nodeInfo.SetNode(node)
+}
