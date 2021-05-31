d0f06026cb95180d2f024d83cf7103398f0ef1e3
Benjamin Pineau <benjamin.pineau@datadoghq.com>
2021-05-31 23:58:09 +0200

    [local] Set local-data custom resource on fresh nodes
    This is meant to replace a patch that was setting new nodes as NotReady
    until their lvp pod was there, with something bound to and contained in
    our podsListProcessor, not touching autoscaler core or cloudproviders at
    all anymore. Decision is entirely based of the local-data:true label: no
    guessing or per cloud provider instances types allowlists needed anymore.
    
    Instead of setting them NotReady, the new nodes that just joined are now
    considered as schedulable for pods requesting local-data once they are
    ready, which naturaly prevents spurious re-upscales.
    
    For now the change is restricted to new local-data nodes that just became
    ready for less than 5mn, as we're assessing wider impact.
    
    The downside is we need to modify the clusterSnapshot content before
    filterOutSchedulable runs scheduler predicates with those nodes, which
    happens later, also in our own podsListProcessor.

--- cluster-autoscaler/processors/datadog/pods/processor.go
+++ cluster-autoscaler/processors/datadog/pods/processor.go
@@ -32,6 +32,7 @@ import (
 func NewFilteringPodListProcessor(stopCtx stdcontext.Context, nodeFilter func(*framework.NodeInfo) bool) *pods.CombinedPodListProcessor {
 	return pods.NewCombinedPodListProcessor([]pods.PodListProcessor{
 		NewTransformLocalData(stopCtx),
+		NewTransformDataNodes(),
 		NewFilterOutLongPending(),
 		podlistprocessor.NewClearTPURequestsPodListProcessor(),
 		podlistprocessor.NewFilterOutExpendablePodListProcessor(),
