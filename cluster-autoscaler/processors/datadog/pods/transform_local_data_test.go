fda018bfff17dda21c373df1fa6da87740c11a57
Benjamin Pineau <benjamin.pineau@datadoghq.com>
2021-05-31 23:04:46 +0200

    [local] Define Datadog custom variables and accesors
    This is meant to isolate, contain and wrap access to our custom
    resource (storageclass/local-data) and label (/local-storage)
    in a single place, separated in our processors/datadog/ namespace.
    
    Avoid spreading local names and constraints over all the code base.

--- cluster-autoscaler/processors/datadog/pods/transform_local_data_test.go
+++ cluster-autoscaler/processors/datadog/pods/transform_local_data_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package pods
 
 import (
@@ -7,9 +23,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	apiequality "k8s.io/apimachinery/pkg/api/equality"
-	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/autoscaler/cluster-autoscaler/context"
+	"k8s.io/autoscaler/cluster-autoscaler/processors/datadog/common"
 	kube_util "k8s.io/autoscaler/cluster-autoscaler/utils/kubernetes"
 	v1lister "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
@@ -21,7 +37,7 @@ var (
 	testNamespace      = "foons"
 	testEmptyResources = corev1.ResourceList{}
 	testLdResources    = corev1.ResourceList{
-		"storageclass/local-data": *resource.NewQuantity(1, resource.DecimalSI),
+		common.DatadogLocalDataResource: common.DatadogLocalDataQuantity.DeepCopy(),
 	}
 )
 
