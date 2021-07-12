aacf59e6c22ccbdbaefabb8d3cf571d04739262b
Benjamin Pineau <benjamin.pineau@datadoghq.com>
2021-07-12 08:55:07 +0200

    [local] Fix scheduler framework import path
    This changed in newer cluster-autoscaler versions.

--- cluster-autoscaler/processors/datadog/common/common_test.go
+++ cluster-autoscaler/processors/datadog/common/common_test.go
@@ -23,7 +23,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	schedulerframework "k8s.io/kubernetes/pkg/scheduler/framework/v1alpha1"
+	schedulerframework "k8s.io/kubernetes/pkg/scheduler/framework"
 )
 
 func TestNodeHasLocalData(t *testing.T) {
