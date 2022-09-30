1d9e5a2db06d9c02ea9631fffb1c7c386ef671b3
lally.singh <lally.singh@datadoghq.com>
2022-09-30 13:02:30 -0400

    Copyright assignment boilerplate.

--- vertical-pod-autoscaler/pkg/utils/qos/rounding.go
+++ vertical-pod-autoscaler/pkg/utils/qos/rounding.go
@@ -1,3 +1,21 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package qos
+
 // QoS Logic.
 //- Annotation analysis for determining if/when we use QoS mode
 //- Rounding:
@@ -6,8 +24,6 @@
 //  - With modulo=N support
 //- Interpolation:
 //  - Fitting within the rounding rules.
-
-package qos
 
 import (
 	v1 "k8s.io/api/core/v1"
