be65dd6fc6154e6b2af26611ed8e4f5eca6fd90e
David Benque <david.benque@datadoghq.com>
2023-02-02 16:25:31 +0100

    Revert "pointer receiver"
    This reverts commit 35e5990fb54285bcf41d147c89572a6eeb28c3c5.

--- vertical-pod-autoscaler/pkg/recommender/input/metrics/metrics_datadog_client.go
+++ vertical-pod-autoscaler/pkg/recommender/input/metrics/metrics_datadog_client.go
@@ -57,13 +57,13 @@ type ddclientPodMetrics struct {
 	ExtraTagsClause string // Something to shove into the query, where it'll add more AND clauses.
 }
 
-func (d *ddclientMetrics) PodMetricses(namespace string) resourceclient.PodMetricsInterface {
+func (d ddclientMetrics) PodMetricses(namespace string) resourceclient.PodMetricsInterface {
 	klog.Infof("ddclientMetrics.PodMetricses(namespace:%s)", namespace)
 	return &ddclientPodMetrics{ddConfig: d.ddConfig, Namespace: namespace, Client: d.Client,
 		QueryInterval: d.QueryInterval, ClusterName: d.ClusterName, ExtraTagsClause: d.ExtraTagsClause}
 }
 
-func (d *ddclientPodMetrics) queryMetrics(ctx context.Context, queryStr string) (datadog.MetricsQueryResponse, error) {
+func (d ddclientPodMetrics) queryMetrics(ctx context.Context, queryStr string) (datadog.MetricsQueryResponse, error) {
 	ctxWithCredential, err := getCtx(ctx, d.ddConfig)
 	if err != nil {
 		return datadog.MetricsQueryResponse{}, err
@@ -156,7 +156,7 @@ func makeResourceList(cpu float64, mem float64) map[v1.ResourceName]resource.Qua
 // timestamp on both cpu and rss.  As we're doing periodic sampling, that's fine.  The most
 // recent timestamp may be partial data.
 // Presumes that all values are for the same pod (tag: pod_name).
-func (d *ddclientPodMetrics) aggregatePodMetrics(podName string, cpuResp []datadog.MetricsQueryMetadata,
+func (d ddclientPodMetrics) aggregatePodMetrics(podName string, cpuResp []datadog.MetricsQueryMetadata,
 	memResp []datadog.MetricsQueryMetadata) *v1beta1.PodMetrics {
 	// Go by container.
 	containersMem := classifyByTag(memResp, "container_name", "unknown-container")
@@ -227,7 +227,7 @@ FindTimestamp:
 
 }
 
-func (d *ddclientPodMetrics) Get(ctx context.Context, podName string, _ metav1.GetOptions) (*v1beta1.PodMetrics, error) {
+func (d ddclientPodMetrics) Get(ctx context.Context, podName string, _ metav1.GetOptions) (*v1beta1.PodMetrics, error) {
 	nsClause := ""
 	if len(d.Namespace) > 0 {
 		nsClause = fmt.Sprintf(" AND kube_namespace:%s ", d.Namespace)
@@ -246,7 +246,7 @@ func (d *ddclientPodMetrics) Get(ctx context.Context, podName string, _ metav1.G
 	return d.aggregatePodMetrics(podName, cpuResp.GetSeries(), memResp.GetSeries()), nil
 }
 
-func (d *ddclientPodMetrics) List(ctx context.Context, _ metav1.ListOptions) (*v1beta1.PodMetricsList, error) {
+func (d ddclientPodMetrics) List(ctx context.Context, _ metav1.ListOptions) (*v1beta1.PodMetricsList, error) {
 	nsClause := ""
 	if len(d.Namespace) > 0 {
 		nsClause = fmt.Sprintf(" AND kube_namespace:%s ", d.Namespace)
@@ -295,7 +295,7 @@ func newDatadogClientWithFactory(queryInterval time.Duration, cluster string, ex
 		}
 
 	}
-	return &ddclientMetrics{Client: apiClient, QueryInterval: -queryInterval, ClusterName: cluster, ExtraTagsClause: clause}
+	return ddclientMetrics{Client: apiClient, QueryInterval: -queryInterval, ClusterName: cluster, ExtraTagsClause: clause}
 }
 
 type clientWrapper struct {
