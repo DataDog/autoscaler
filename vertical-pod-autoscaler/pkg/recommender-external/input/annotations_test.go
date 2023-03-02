efca0a0b9d02f32fd5fe8a82bd68d42afd35d596
Corentin Chary <corentin.chary@datadoghq.com>
2023-03-02 09:59:36 +0100

    Address review comments

--- vertical-pod-autoscaler/pkg/recommender-external/input/annotations_test.go
+++ vertical-pod-autoscaler/pkg/recommender-external/input/annotations_test.go
@@ -26,27 +26,22 @@ import (
 
 func TestGetVpaExternalMetrics(t *testing.T) {
 	type args struct {
-		annotations map[string]string
 	}
 	tests := []struct {
-		name string
-		args args
-		want ContainersToResourcesAndMetrics
+		name        string
+		annotations map[string]string
+		want        ContainersToResourcesAndMetrics
 	}{
 		{
-			name: "empty",
-			args: args{
-				annotations: map[string]string{},
-			},
-			want: ContainersToResourcesAndMetrics{},
+			name:        "empty",
+			annotations: map[string]string{},
+			want:        ContainersToResourcesAndMetrics{},
 		},
 		{
 			name: "some annotations",
-			args: args{
-				annotations: map[string]string{
-					AnnotationKey("container1", model.ResourceCPU):    "system-cpu",
-					AnnotationKey("container1", model.ResourceMemory): "system-memory",
-				},
+			annotations: map[string]string{
+				AnnotationKey("container1", model.ResourceCPU):    "system-cpu",
+				AnnotationKey("container1", model.ResourceMemory): "system-memory",
 			},
 			want: ContainersToResourcesAndMetrics{
 				"container1": {
@@ -58,7 +53,7 @@ func TestGetVpaExternalMetrics(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equalf(t, tt.want, GetVpaExternalMetrics(tt.args.annotations), "GetVpaExternalMetrics(%v)", tt.args.annotations)
+			assert.Equalf(t, tt.want, GetVpaExternalMetrics(tt.annotations), "GetVpaExternalMetrics(%v)", tt.annotations)
 		})
 	}
 }
